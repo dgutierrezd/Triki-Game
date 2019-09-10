56ff261b5074bf8214533936f63074dc3928f02d
daniel gutierrez <danielgudu22@gmail.com>
2019-09-09 21:46:36 -0500

    Se odifican los botones y se genera un numero random para cpu

--- triki.py
+++ triki.py
@@ -13,38 +13,54 @@ Created on Mon Sep  9 19:40:55 2019
 @author: daniel
 """
 
-from tkinter import *
+from functools import partial
+import random
+import tkinter as tk
 
-def graphic():
-    root = Tk()
+root = tk.Tk()
 
-    topFrame = Frame(root)
-    topFrame.pack()
-    middleFrame = Frame(root)
-    middleFrame.pack()
-    bottomFrame = Frame(root)
-    bottomFrame.pack()
+topFrame = tk.Frame(root)
+topFrame.pack()
+middleFrame = tk.Frame(root)
+middleFrame.pack()
+bottomFrame = tk.Frame(root)
+bottomFrame.pack()
+
+def graphicTriki(estado):
+    space1 = tk.Button(topFrame, text='1', height = 10, width = 10, command=lambda: jugadaPlayer(space1, True))
+    space2 = tk.Button(topFrame, text='2', height = 10, width = 10, command=lambda: jugadaPlayer(space2, True))
+    space3 = tk.Button(topFrame, text='3', height = 10, width = 10, command=lambda: jugadaPlayer(space3, True))
+    space4 = tk.Button(middleFrame, text='4', height = 10, width = 10, command=lambda: jugadaPlayer(space4, True))
+    space5 = tk.Button(middleFrame, text='5', height = 10, width = 10, command=lambda: jugadaPlayer(space5, True))
+    space6 = tk.Button(middleFrame, text='6', height = 10, width = 10, command=lambda: jugadaPlayer(space6, True))
+    space7 = tk.Button(bottomFrame, text='7', height = 10, width = 10, command=lambda: jugadaPlayer(space7, True))
+    space8 = tk.Button(bottomFrame, text='8', height = 10, width = 10, command=lambda: jugadaPlayer(space8, True))
+    space9 = tk.Button(bottomFrame, text='9', height = 10, width = 10, command=lambda: jugadaPlayer(space9, True))
     
-    space1 = Button(topFrame, text='1', height = 10, width = 10)
-    space2 = Button(topFrame, text='2', height = 10, width = 10)
-    space3 = Button(topFrame, text='3', height = 10, width = 10)
-    space4 = Button(middleFrame, text='4', height = 10, width = 10)
-    space5 = Button(middleFrame, text='5', height = 10, width = 10)
-    space6 = Button(middleFrame, text='6', height = 10, width = 10)
-    space7 = Button(bottomFrame, text='7', height = 10, width = 10)
-    space8 = Button(bottomFrame, text='8', height = 10, width = 10)
-    space9 = Button(bottomFrame, text='9', height = 10, width = 10)
+    #label1.pack(side=tk.LEFT)
+    space1.pack(side=tk.LEFT)
+    space2.pack(side=tk.LEFT)
+    space3.pack(side=tk.LEFT)
+    space4.pack(side=tk.LEFT)
+    space5.pack(side=tk.LEFT)
+    space6.pack(side=tk.LEFT)
+    space7.pack(side=tk.LEFT)
+    space8.pack(side=tk.LEFT)
+    space9.pack(side=tk.LEFT)
+    tk.Label(root, text="Tu eres la letra X, la CPU es la letra O \nEscoge donde quieres hacer tu jugada.").pack()
     
-    space1.pack(side=LEFT)
-    space2.pack(side=LEFT)
-    space3.pack(side=LEFT)
-    space4.pack(side=LEFT)
-    space5.pack(side=LEFT)
-    space6.pack(side=LEFT)
-    space7.pack(side=LEFT)
-    space8.pack(side=LEFT)
-    space9.pack(side=LEFT)
     
-    root.mainloop()
-    
-graphic()
+def jugadaPlayer(boton, estado):
+    if estado is True:
+        boton['text'] = X
+        boton['state'] = DISABLED
+        jugadaCPU()
+    else:
+        return boton
+
+def jugadaCPU():
+    randomNum = random.randint(1,9)
+    print(randomNum)
+
+graphicTriki(False)
+tk.mainloop()
